d46d01f9aa69f476d113ca14f3119e3f9a46c795
Tizian Schmidlin <st@cabag.ch>
2018-08-09 08:23:22 +0200

    [BUGFIX][MAINTENANCE]Fix namespace for Hooksubscriber
    Also add some fixes suggested by PHP Storm.

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = array (
   'modify_tables' => '',
   'clearCacheOnLoad' => 1,
   'lockType' => '',
-  'version' => '5.2.0',
+  'version' => '5.2.1',
   'constraints' => 
   array (
     'depends' => 
